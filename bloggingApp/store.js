5d097a9e17ae15c31e0110ca044d46b9a48f7575
alexTenche <alex.tenche@gmail.com>
2015-01-25 14:30:29 +0200

    progress on bloggingApp with Ember

---
+++ bloggingApp/store.js
@@ -0,0 +1,45 @@
+var posts = [
+	{
+		id: '1',
+		title: 'Title 1',
+		body: 'Un numar crescut de persoane mai ales din vestul Europei si Balcanii de Vest sunt interesate sa plece in Siria si Irak pentru a particpa la conflictele de acolo, tranzitand, cu predilectie, Romania si Bulgaria, ca rute terestre, afirma, sambata, Serviciul Roman de Informatii (SRI), care sustine ca "gestionarea eficienta a riscurilor asociate acestui fenomen este extrem" de dificila, fara unele "instrumente legale".'
+	},
+	{
+		id: '2',
+		title: 'Title 2',
+		body: 'Purtatorul de cuvant al PNL, Ionut Stroe, a declarat, sambata, pentru Mediafax, ca Statutul PNL se va aplica "invariabil pentru toti, in aceeasi masura", in cazul dosarelor de coruptie, facand referire la excluderea prin radiere a lui Tudor Chiuariu si la cazurile deputatilor Thuma Hubert si Verginel Gireada.'
+	},
+	{
+		id: '3',
+		title: 'Title 3',
+		body: 'Purtatorul de cuvant al PNL, Ionut Stroe, a declarat, sambata, pentru Mediafax, ca Statutul PNL se va aplica "invariabil pentru toti, in aceeasi masura", in cazul dosarelor de coruptie, facand referire la excluderea prin radiere a lui Tudor Chiuariu si la cazurile deputatilor Thuma Hubert si Verginel Gireada.'
+	}
+];
+
+var comments = [
+	{
+		id: '1',
+		postId: '3',
+		text: 'Neeee'
+	},
+	{
+		id: '2',
+		postId: '3',
+		text: 'yeeeeee'
+	},
+	{
+		id: '3',
+		postId: '1',
+		text: 'nooooooo'
+	},
+	{
+		id: '4',
+		postId: '1',
+		text: 'booooo'
+	},
+	{
+		id: '5',
+		postId: '2',
+		text: 'Neeee'
+	}
+]
